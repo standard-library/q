33c501350525acc1b0acd1ebfab7ff74a657b3d2
Ryan <ryan@superpositionkitty.com>
2015-05-01 12:47:01 +0100

    show when tests end

--- test/index.js
+++ test/index.js
@@ -1,12 +1,16 @@
 var test = require('tape')
 var q = require('../')
 
+console.log(q('ul li')[0] === q('ul li:first-of-type'))
+
 test('test q', function (t) {
   var ul = q('ul')
   var li
   t.equal(q('ul li').length, 3)
-  t.deepEqual(q('ul li'), q('li', ul))
   t.equal(q('ul li')[0].textContent, '$1')
+  t.deepEqual(q('ul li:first-of-type'), q('ul li')[0])
+  t.deepEqual(q('ul li'), q('li', ul))
   t.deepEqual(q('ul div'), [])
   t.deepEqual(q('div', li), [])
+  t.end()
 })

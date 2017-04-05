5cd4bd4b3671d112ec59f70ffb1a3598ec33431b
Ryan <ryan@graphicalhouse.com>
2017-04-05 21:33:34 +0100

    make sure to use optional arguments with arg?: type not arg: ?type

--- index.es6.js
+++ index.es6.js
@@ -7,15 +7,15 @@ const elemError = (e: mixed): void => {
   throw new Error(`"${String(e)}" does\'t exist in the document`)
 }
 
-const getRoot = (e: ?HTMLElement): ?(Document | HTMLElement) => (
+const getRoot = (e?: HTMLElement): ?(Document | HTMLElement) => (
   !e ? document : (document && document.body && document.body.contains(e) ? e : elemError(e))
 )
 
-export const query = (q: string, e: ?HTMLElement): HTMLElement[] => {
+export const query = (q: string, e?: HTMLElement): HTMLElement[] => {
   const root = getRoot(e)
   return root ? toArray(root.querySelectorAll(q)) : []
 }
 
-export const queryOne = (q: string, e: ?HTMLElement): ?HTMLElement => (
+export const queryOne = (q: string, e?: HTMLElement): ?HTMLElement => (
   first(query(q, e))
 )
